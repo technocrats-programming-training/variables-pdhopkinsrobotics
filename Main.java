d525f5cf1af3982b0f1956bd585b666d2badbffc
Paul Dirkswager <107653146+pdhopkinsrobotics@users.noreply.github.com>
2022-06-23 22:38:55 +0000

    Almost done, but console froze

--- Main.java
+++ Main.java
@@ -9,13 +9,33 @@ public class Main {
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
+        String firstMessage = "Hello, I am " + firstName + " " + lastName + ", and I am " + age + "years old.";
+
+        // AGE
+        int checkEven = age % 2;
+        boolean isEven = checkEven == 0;
+        String secondMessage;
+      
+        if (isEven) {secondMessage = "My age is even.";}
+        else
+            {secondMessage = "My age is NOT even.";}
         
-
+      // NAMES
+        boolean isNamesSame = firstName == lastName;
+        
+        String thirdMessage;
+      
+        if (isNamesSame) {thirdMessage = "The names are the same.";}
+        else
+            {thirdMessage = "The names are NOT the same.";}
+        
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
-
+        System.out.println (firstMessage);
         // Print out whether age is even
-
+        //System.out.println (checkEven);
+        System.out.println (secondMessage);
         // Print out whether firstName equals lastName
+        System.out.println (thirdMessage);
 
         // Print out whether age is greater than 0
 
